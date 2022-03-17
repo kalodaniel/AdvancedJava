f9cb98c9bcd29ac8ea37fb616e9a74d71700bed6
kalodaniel <72191543+kalodaniel@users.noreply.github.com>
2022-03-17 11:26:25 +0100

    Add files via upload

---
+++ mini-neptun-main/src/test/java/org/example/TeacherServiceImplTest.java
@@ -0,0 +1,93 @@
+package org.example;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.*;
+
+public class TeacherServiceImplTest {
+
+    @Test
+    public void getMyStudents() {
+        // GIVEN
+        Long teacherId = 234L;
+
+        CourseRepository courseRepository = mock(CourseRepository.class);
+        List<CourseDto> findByTeacherResult = new ArrayList<>();
+        findByTeacherResult.add(new CourseDto(1L, "demo course", new ArrayList()));
+        when(courseRepository.findByTeacher(teacherId)).thenReturn(findByTeacherResult);
+
+        StudentRepository studentRepository = mock(StudentRepository.class);
+        List<StudentDto> findByCourseIdResult = new ArrayList();
+        findByCourseIdResult.add( new StudentDto(1L, "name 1", "nc 1"));
+        findByCourseIdResult.add( new StudentDto(2L, "name 2", "nc 2"));
+        findByCourseIdResult.add( new StudentDto(3L, "name 3", "nc 3"));
+        when(studentRepository.findByCourseId(anyList())).thenReturn(findByCourseIdResult);
+
+        TeacherServiceImpl teacher = new TeacherServiceImpl(studentRepository, courseRepository, teacherId);
+
+        // WHEN
+        List<StudentDto> myStudents = teacher.getMyStudents();
+
+        // THEN
+        assertEquals(myStudents.size(), 3);
+
+        verify(courseRepository, times(1)).findByTeacher(eq(teacherId));
+        verify(studentRepository, times(1)).findByCourseId(anyList());
+    }
+
+    @Test
+    public void shouldBeTired_TooMuchCourse() {
+        // GIVEN
+        Long teacherId = 234L;
+        int numberOfCourses = 125;
+        CourseRepository courseRepository = mock(CourseRepository.class);
+        when(courseRepository.getElementCount(teacherId)).thenReturn(numberOfCourses);
+        StudentRepository studentRepository = mock(StudentRepository.class);
+        TeacherServiceImpl teacher = new TeacherServiceImpl(studentRepository, courseRepository, teacherId);
+
+        // WHEN THEN
+        assertTrue(teacher.shouldBeTired());
+    }
+
+    @Test
+    public void shouldBeTired_No() {
+        // GIVEN
+        Long teacherId = 234L;
+        CourseRepository courseRepository = mock(CourseRepository.class);
+        StudentRepository studentRepository = mock(StudentRepository.class);
+        when(courseRepository.getElementCount(teacherId)).thenReturn(1);
+        TeacherServiceImpl teacher = new TeacherServiceImpl(studentRepository, courseRepository, teacherId);
+
+        // WHEN THEN
+        assertFalse(teacher.shouldBeTired());
+    }
+
+    @Test
+    public void shouldBeTired_TooMuchUsers() {
+        // GIVEN
+        Long teacherId = 234L;
+        CourseRepository courseRepository = mock(CourseRepository.class);
+        when(courseRepository.getElementCount(teacherId)).thenReturn(1);
+
+        StudentRepository studentRepository = mock(StudentRepository.class);
+        when(studentRepository.findByCourseId(any())).thenReturn(getStudentListElevenItem());
+
+        TeacherServiceImpl teacher = new TeacherServiceImpl(studentRepository, courseRepository, teacherId);
+
+        // WHEN THEN
+        assertTrue(teacher.shouldBeTired());
+    }
+
+    private List<StudentDto> getStudentListElevenItem(){
+        List<StudentDto> studentList = new ArrayList<>();
+        for (long i = 0; i < 11; i++){
+            studentList.add(new StudentDto(i,"name "+i,"neptun "+i));
+        }
+        return studentList;
+    }
+}
